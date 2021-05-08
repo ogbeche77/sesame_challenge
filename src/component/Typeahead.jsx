0fb25046909a63dddae9031116cdd6171b620832
ogbeche77 <ogbeche77@yahoo.com>
2021-05-08 11:57:09 +0200

    change component title

--- src/component/Typeahead.jsx
+++ src/component/Typeahead.jsx
@@ -51,7 +51,7 @@ const Typeahead = () => {
   return (
     <>
       <div>
-        <h1 className="title">Sesame FrontEnd Challenge</h1>
+        <h1 className="title">Type Ahead Component</h1>
       </div>
       <div className="wrapper">
         <input
